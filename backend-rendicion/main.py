80475c93952e4b5c9308fd9b23ed129af61b718c
EdsonPinto <epintoabreo@gmail.com>
2026-09-04 00:19:27 -0500

    Refactor: remove obsolete main wrapper

--- backend-rendicion/main.py
+++ backend-rendicion/main.py
@@ -1,10 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import pandas as pd
 
-import app_state
 from config import CORS_ORIGINS
-from services.estadisticas_service import generar_reporte as generar_reporte_estadisticas
 from routers.admin_router import router as admin_router
 from routers.auth_router import router as auth_router
 from routers.comparativa_router import router as comparativa_router
@@ -32,7 +29,3 @@ app.include_router(comparativa_router)
 app.include_router(cuello_botella_router)
 app.include_router(export_router)
 app.include_router(mapeos_router)
-
-
-def generar_reporte(df_base: pd.DataFrame) -> dict:
-    return generar_reporte_estadisticas(df_base, app_state.meta)
